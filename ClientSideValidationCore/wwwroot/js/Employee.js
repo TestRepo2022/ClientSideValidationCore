6e0ed674dbaf93cf3a2b20fdfe5e2c7c55839f3a
TestRepo2022 <demo3408@gmail.com>
2022-10-09 12:16:23 +0530

    Add project files.

---
+++ ClientSideValidationCore/wwwroot/js/Employee.js
@@ -0,0 +1,44 @@
+﻿$(document).ready(function () {
+
+    $("#btnSubmit").click(function () {
+        isValid = requiredFiled("FName", "first name","all");
+        if (!isValid) { return false }
+
+        isValid = requiredFiled("LName", "last name","all");
+        if (!isValid) { return false }
+
+        isValid = requiredFiled("Email", "email address","email");
+        if (!isValid) { return false }
+
+        isValid = requiredFiled("Mobile", "mobile number","mobile");
+        if (!isValid) { return false }
+
+        isValid = requiredSelectFiled("Gender", "gender");
+        if (!isValid) { return false }
+
+        isValid = requiredFiled("Salary", "last salary", "numeric");
+        if (!isValid) { return false }
+
+
+        if (isValid) {
+            alert("form is valid")
+        }
+    })
+
+})
+
+
+$("#txtFName").keyup(function () {
+    isValid = requiredFiled("FName", "first name","all");
+    if (!isValid) { return false }
+})
+
+$("#txtLName").keyup(function () {
+    isValid = requiredFiled("LName", "last name","all");
+    if (!isValid) { return false }
+})
+
+$("#ddlGender").change(function () {
+    isValid = requiredSelectFiled("Gender", "gender");
+    if (!isValid) { return false }
+})
---
+++ ClientSideValidationCore/wwwroot/js/Utility.js
@@ -0,0 +1,63 @@
+﻿
+function requiredFiled(control, message, type) {
+    var Id = "#txt" + control;
+    var err = "#err" + control
+    var group = "#formGroup" + control;
+    if ($(Id).val() == "" || $(Id).val() == null || $(Id).val() == undefined) {
+
+        $(Id).addClass("error-control")
+        $(err).html("Please enter " + message)
+        return false;
+    }
+    else {
+        if (ValidationType(type).test($(Id).val())) {
+            $(Id).removeClass("error-control")
+            $(err).html("")
+            return true;
+        }
+        else {
+            $(Id).addClass("error-control")
+            $(err).html("Please enter valid " + message)
+            return false;
+        }
+    }
+}
+
+function requiredSelectFiled(control, message) {
+    var Id = "#ddl" + control;
+    var err = "#err" + control
+    var group = "#formGroup" + control;
+    if ($(Id).val() == "" || $(Id).val() == null || $(Id).val() == "-1") {
+
+        $(Id).addClass("error-control")
+        $(err).html("Please select " + message)
+        return false;
+    }
+    else {
+        $(Id).removeClass("error-control")
+        $(err).html("")
+        return true;
+    }
+}
+
+function ValidationType(type) {
+    var regx = "";
+    switch (type) {
+        case "email":
+            regx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            break;
+
+        case "mobile":
+            regx = /^([0|\+[0-9]{1,5})?([7-9][0-9]{9})$/;
+            break;
+
+        case "numeric":
+            regx = /^\d+$/;
+            break;
+        default:
+            regx = /(.*?)/
+            break;
+    }
+    return regx;
+}
+
